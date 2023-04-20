3313ea2b4d21ef4fbfd2559d30f82bb48a77f0d6
ariel <santa.cruz.unla@gmail.com>
2023-04-20 16:11:45 -0300

    Agregar archivos de proyecto.

---
+++ eventos 1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace eventos_1
+{
+    public partial class Form1 : Form
+    {
+        Termostato t;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t = new Termostato();
+            t.TemperaturaPeligrosa += FuncionSuscriptaTemperaturaPeligrosa; // suscribo el eventop
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string temp = Interaction.InputBox("ingrese temp");
+            try
+            {
+                t.Temperatura = int.Parse(temp);
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+                
+        }
+        private void FuncionSuscriptaTemperaturaPeligrosa(object sender, EventArgs e)
+        {
+            MessageBox.Show("temp peligrosa");
+        } 
+    }
+
+    public class Termostato 
+    {
+        private int temperatura;
+
+        //declaracion del evento
+        public event EventHandler TemperaturaPeligrosa;
+        //metodo
+        public int Temperatura
+        {
+            get { return temperatura; }
+            //desencadenamiento del evento
+            set { temperatura = value; if (value > 100) TemperaturaPeligrosa?.Invoke(null, null); }
+            // if (TEmperaturaPeligrosa!=null){temperaturaPeligrosa(null,null);}
+        }
+
+    }
+
+
+}
---
+++ itinerario 3/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace itinerario_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public abstract class Mamifero 
+    {
+        #region metodos
+        public abstract string Comer();
+        public virtual string QuienSoy() { return "soy un mamifero"; }
+        // los metodos virtuales aceptan implementacion en el metodo abstracto
+        // tambien se pueden sobreecribir
+        #endregion
+    }
+
+    public class Vaca : Mamifero
+    {
+        public override string Comer()
+        {
+            return "como como un rumiante";
+        }
+
+        public override string QuienSoy()
+        {
+            return "soy una vaca";
+        }
+    }
+
+    public class Persona : Mamifero
+    {
+        public override string Comer()
+        {
+            return "como como una persona";
+        }
+    }
+
+    public sealed class Perro : Mamifero
+    {
+        public override string Comer()
+        {
+            return "come como un perro";
+        }
+    }
+
+    public abstract class Cetaceo: Mamifero
+    {
+
+    }
+}
